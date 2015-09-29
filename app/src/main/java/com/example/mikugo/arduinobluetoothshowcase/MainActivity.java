ec8eecb5d6283d54619d93f2691a0c06e85c488b
Goran Mikulic <mikulicg@gmail.com>
2015-09-29 21:39:38 +0200

    querying paired devices

--- app/src/main/java/com/example/mikugo/arduinobluetoothshowcase/MainActivity.java
+++ app/src/main/java/com/example/mikugo/arduinobluetoothshowcase/MainActivity.java
@@ -1,16 +1,57 @@
 package com.example.mikugo.arduinobluetoothshowcase;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter mBluetoothAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    /**
+     * Initializes bluetooth adapter and checks if bluetooth is enabled
+     */
+    private void initBluetooth() {
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(MainActivity.this, "Device does not support Bluetooth", Toast.LENGTH_SHORT).show();
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+
+        initBluetooth();
+
     }
 
     @Override
@@ -30,8 +71,34 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.action_search_devices) {
+            queryPairedDevices();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Queries devices which already were paired with that smartphone
+     */
+    private void queryPairedDevices() {
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        List<String> devices = new ArrayList<>();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                devices.add(device.getName() + "\n" + device.getAddress());
+            }
+            System.out.println(devices);
+
+        }
+    }
+
+    private Set<BluetoothDevice> getPairedDevices() {
+
+        return mBluetoothAdapter.getBondedDevices();
+
     }
 }
